fba3d206d78d52ab47bfc4490349b5f7cde3c358
Jorge052011 <jimunozacuna+1@gmail.com>
2025-04-28 03:16:20 +0000

    terminado falta color icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,15 @@ const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstra
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+$("#enviarcorreo").click(function() {
+    alert("El correo fue enviado correctamente");
+})
+
+$('h2').dblclick(function(){
+    $(this).css('color', 'red')
+})
+
+$('.card-title1').click(function(){
+    $('.card-text1').toggle()
+})
+
